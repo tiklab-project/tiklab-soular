2955c303400658601dbc33747318bafcf8e29d21
马海 <mahai@darthcloud.com>
2021-07-20 10:29:34 +0800

    dist

--- doublekit-portal-ui/src/main/resources/static/js/7.2f2bbf36.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{794:function(n,e,t){"use strict";t.r(e);var o=t(41),a=t.n(o),i=t(0),l=t.n(i),r=t(205);e.default=function(n){return l.a.createElement("div",null,l.a.createElement(r.PortalLogin,a()({},n,{loginGoRouter:"/work",fetchMethod:"get",languageUrl:"/plugin/getPluginConfig"})))}}}]);

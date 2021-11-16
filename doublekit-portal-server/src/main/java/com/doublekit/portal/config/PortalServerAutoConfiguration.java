0079aec82eb1362c39198b629d34a7d9004672ec
zhangzhihua <zhangzhihua@darthcloud.com>
2021-11-16 11:01:29 +0800

    opt datafly

--- doublekit-portal-server/src/main/java/com/doublekit/portal/config/PortalServerAutoConfiguration.java
+++ doublekit-portal-server/src/main/java/com/doublekit/portal/config/PortalServerAutoConfiguration.java
@@ -1,6 +1,6 @@
 package com.doublekit.portal.config;
 
-import com.doublekit.datafly.starter.annotation.DataFly;
+import com.doublekit.datafly.annotation.DataFly;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * PortalServerAutoConfiguration
  */
 @Configuration
-@DataFly(locations = {"scripts/portal.sql"})
+@DataFly(modules = {"portal"})
 @ComponentScan({"com.doublekit.portal"})
 public class PortalServerAutoConfiguration {
 }

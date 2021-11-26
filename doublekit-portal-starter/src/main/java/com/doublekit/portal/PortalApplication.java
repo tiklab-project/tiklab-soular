dcf7b10c907b23b8888057830fd9935aaa9684af
zhangzhihua <zhangzhihua@darthcloud.com>
2021-11-26 17:15:30 +0800

    refactor git/maven server to local

--- doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalApplication.java
+++ doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 /**

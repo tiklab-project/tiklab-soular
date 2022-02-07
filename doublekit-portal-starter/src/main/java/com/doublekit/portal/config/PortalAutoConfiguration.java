83bc537de8154ff2dd9b1ec00aec611177ddb3c2
zhangzhihua <zhangzhihua@darthcloud.com>
2022-02-07 13:37:48 +0800

    refactor rpc

--- doublekit-portal-starter/src/main/java/com/doublekit/portal/config/PortalAutoConfiguration.java
+++ doublekit-portal-starter/src/main/java/com/doublekit/portal/config/PortalAutoConfiguration.java
@@ -1,8 +1,6 @@
 package com.doublekit.portal.config;
 
 import com.doublekit.apibox.client.annotation.EnableApiboxClient;
-import com.doublekit.datafly.annotation.DataFly;
-import com.doublekit.message.annotation.EnableMessage;
 import com.doublekit.beans.starter.annotation.EnableBeans;
 import com.doublekit.dal.starter.annotation.EnableDal;
 import com.doublekit.datafly.starter.annotation.EnableDataFly;
@@ -10,14 +8,13 @@ import com.doublekit.dcs.starter.annotation.EnableDcs;
 import com.doublekit.dfs.starter.annotation.EnableDfs;
 import com.doublekit.dsl.starter.annotation.EnableDsl;
 import com.doublekit.dss.starter.annotation.EnableDss;
-import com.doublekit.eam.server.annotation.EnableEamServer;
 import com.doublekit.eam.client.annotation.EnableEamClient;
+import com.doublekit.eam.server.annotation.EnableEamServer;
+import com.doublekit.message.annotation.EnableMessage;
 import com.doublekit.plugin.annotation.EnablePluginServer;
 import com.doublekit.portal.annotation.EnablePortalServer;
 import com.doublekit.privilege.annotation.EnablePrivilegeServer;
 import com.doublekit.rpc.starter.annotation.EnableRpc;
-import com.doublekit.rpc.starter.annotation.EnableRpcClient;
-import com.doublekit.rpc.starter.annotation.EnableRpcServer;
 import com.doublekit.toolkit.annotation.EnableToolkitServer;
 import com.doublekit.user.annotation.EnableUserServer;
 import com.doublekit.web.starter.annotation.EnableWeb;

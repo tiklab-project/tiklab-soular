6238cdc079beea5a6b60f56afa372c890c52c642
zhangzhihua <zhangzhihua@darthcloud.com>
2022-04-07 09:42:27 +0800

    删除x工程ce版本，使用原ce工程版本

--- doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalApplication.java
+++ doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -13,7 +12,7 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @EnablePortal
-@PropertySource(value = "classpath:application.properties")
+@PropertySource(value = "classpath:application-${env:dev}.properties")
 public class PortalApplication {
 
     public static final Logger logger = LoggerFactory.getLogger(PortalApplication.class);

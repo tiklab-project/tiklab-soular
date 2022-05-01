0a5535907834d20e47ba3eb33fe534a7aef97f50
zhangzhihua <zhangzhihua@darthcloud.com>
2022-05-01 11:55:43 +0800

    reafctor dss->dis

--- doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalAutoConfiguration.java
+++ doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalAutoConfiguration.java
@@ -6,7 +6,7 @@ import com.doublekit.dal.starter.annotation.EnableDal;
 import com.doublekit.datafly.starter.annotation.EnableDataFly;
 import com.doublekit.dcs.starter.EnableDcs;
 import com.doublekit.dfs.starter.EnableDfs;
-import com.doublekit.dss.starter.EnableDss;
+import com.doublekit.dis.starter.EnableDis;
 import com.doublekit.eam.server.EnableEamServer;
 import com.doublekit.gateway.starter.EnableGateway;
 import com.doublekit.join.starter.annotation.EnableJoin;
@@ -32,7 +32,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableJoin
 @EnableDfs
 @EnableDcs
-@EnableDss
+@EnableDis
 @EnableRpc
 @EnableMessage
 @EnableGateway

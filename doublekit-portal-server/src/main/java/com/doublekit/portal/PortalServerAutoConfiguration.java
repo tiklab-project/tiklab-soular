9027ab652555110364a7bd33a10bd85cfbacb0d6
zhangzhihua <zhangzhihua@darthcloud.com>
2022-05-12 10:47:40 +0800

    删除portal client/server依赖

--- doublekit-portal-server/src/main/java/com/doublekit/portal/PortalServerAutoConfiguration.java
+++ doublekit-portal-server/src/main/java/com/doublekit/portal/PortalServerAutoConfiguration.java
@@ -1,8 +1,6 @@
 package com.doublekit.portal;
 
 import com.doublekit.dsm.annotation.SQL;
-import com.doublekit.eam.server.EnableEamServer;
-import com.doublekit.user.EnableUserServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +8,6 @@ import org.springframework.context.annotation.Configuration;
  * PortalServerAutoConfiguration
  */
 @Configuration
-@EnableUserServer
-@EnableEamServer
 @SQL(modules = {"portal"})
 @ComponentScan({"com.doublekit.portal"})
 public class PortalServerAutoConfiguration {
--- doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalAutoConfiguration.java
+++ doublekit-portal-starter/src/main/java/com/doublekit/portal/PortalAutoConfiguration.java
@@ -5,7 +5,7 @@ import com.doublekit.beans.starter.annotation.EnableBeans;
 import com.doublekit.dal.starter.annotation.EnableDal;
 import com.doublekit.dsm.starter.annotation.EnableDsm;
 import com.doublekit.dss.starter.EnableDss;
-import com.doublekit.eam.server.EnableEamServer;
+import com.doublekit.eam.starter.EnableEam;
 import com.doublekit.gateway.starter.EnableGateway;
 import com.doublekit.join.starter.annotation.EnableJoin;
 import com.doublekit.licence.EnableLicenceServer;
@@ -14,7 +14,7 @@ import com.doublekit.pluginx.EnablePluginServer;
 import com.doublekit.privilege.EnablePrivilegeServer;
 import com.doublekit.rpc.starter.annotation.EnableRpc;
 import com.doublekit.toolkit.EnableToolkitServer;
-import com.doublekit.user.EnableUserServer;
+import com.doublekit.user.starter.EnableUser;
 import com.doublekit.web.starter.annotation.EnableWeb;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,6 +33,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableMessage
 @EnableGateway
 //pcs
+@EnableUser
+@EnableEam
 @EnablePrivilegeServer
 @EnablePluginServer
 @EnableToolkitServer

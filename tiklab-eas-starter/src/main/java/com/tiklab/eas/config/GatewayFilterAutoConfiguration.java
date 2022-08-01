364e22cdec76ca534d356678214ceecf4a02acba
zhangzhihua <zhangzhihua@darthcloud.com>
2022-08-01 15:09:37 +0800

    refactor gateway

--- tiklab-eas-starter/src/main/java/com/tiklab/eas/config/GatewayFilterAutoConfiguration.java
+++ tiklab-eas-starter/src/main/java/com/tiklab/eas/config/GatewayFilterAutoConfiguration.java
@@ -1,12 +1,32 @@
 package com.tiklab.eas.config;
 
+import com.tiklab.eam.author.Authenticator;
 import com.tiklab.eam.server.author.config.IgnoreConfig;
 import com.tiklab.eam.server.author.config.IgnoreConfigBuilder;
+import com.tiklab.eam.server.handler.AuthorHandler;
+import com.tiklab.gateway.GatewayFilter;
+import com.tiklab.gateway.router.RouterHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayFilterAutoConfiguration {
+
+    //网关filter
+    @Bean
+    GatewayFilter gatewayFilter(AuthorHandler authorHandler){
+        return new GatewayFilter()
+                .addHandler(authorHandler);
+    }
+
+    //认证handler
+    @Bean
+    AuthorHandler authorHandler(Authenticator authenticator, IgnoreConfig ignoreConfig){
+        return new AuthorHandler()
+                .setAuthenticator(authenticator)
+                .setIgnoreConfig(ignoreConfig);
+
+    }
 
     @Bean
     public IgnoreConfig ignoreConfig(){

941bbb8bb1a969765e025a30b57acf25774815c1
mahai <mahai@darthcloud.com>
2022-09-07 10:21:03 +0800

    移出社区版licence

--- tiklab-eas-enable-starter/src/main/java/com/tiklab/eas/enable/EasInlineAutoConfiguration.java
+++ tiklab-eas-enable-starter/src/main/java/com/tiklab/eas/enable/EasInlineAutoConfiguration.java
@@ -9,7 +9,6 @@ import com.tiklab.dss.starter.EnableDss;
 import com.tiklab.eam.starter.EnableEam;
 import com.tiklab.gateway.starter.EnableGateway;
 import com.tiklab.join.starter.annotation.EnableJoin;
-import com.tiklab.licence.starter.EnableLicenceServer;
 import com.tiklab.message.starter.EnableMessage;
 import com.tiklab.pluginx.starter.EnablePluginServer;
 import com.tiklab.postin.client.EnablePostInClient;
@@ -45,7 +44,6 @@ import org.springframework.context.annotation.Configuration;
 @EnablePrivilegeServer
 @EnablePluginServer
 @EnableToolkitServer
-@EnableLicenceServer
 //other
 @EnablePostInClient
 

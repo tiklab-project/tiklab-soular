541dc17f9df902f61d62da8718a632dad3d3c2da
mahai <mahai@darthcloud.com>
2022-09-09 14:03:17 +0800

    内嵌shell脚本

--- tiklab-eas-enable-starter/bin/startup.sh
+++ tiklab-eas-enable-starter/bin/startup.sh
@@ -29,13 +29,15 @@ JAVA_OPTS="$JAVA_OPTS -Dconf.config=file:${APP_CONFIG}"
 JAVA_opens="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.sql/java.sql=ALL-UNNAMED"
 
 CLASSPATH=${APP_HOME}/conf
+
+
 for appJar in "$APP_HOME"/lib/*.jar;
 do
    CLASSPATH="$CLASSPATH":"$appJar"
 done
 
 
-for appJar in "$DIRS"/tiklab-eas/comment/*.jar;
+for appJar in "$APP_HOME"/comment/*.jar;
 do
    CLASSPATH="$CLASSPATH":"$appJar"
 done
@@ -95,10 +97,7 @@ startup(){
         if [ ! -d "$APP_LOG" ]; then
             mkdir "$APP_LOG"
         fi
-        #nohup $JAVA_HOME/bin/java $JAVA_opens $JAVA_OPTS -classpath $CLASSPATH $APP_MAIN --server.port=8060 --jdbc.url="jdbc:mysql://172.10.1.10:3306/tiklab_eas?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true" --jdbc.username=root --jdbc.password=darth2020 --jdbc.driverClassName="com.mysql.cj.jdbc.Driver" --mysql.embbed.enable=false --mysql.server.port=3306 --eas.mysql.embbed.enable=false --eas.mysql.server.port=3306 --eas.mysql.name="tiklab_eas" --eas.server.port=8060 --eas.jdbc.url="jdbc:mysql://172.10.1.10:3306/tiklab_eas?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true" --eas.jdbc.driverClassName="com.mysql.cj.jdbc.Driver" --eas.jdbc.username=root --eas.jdbc.password=darth2020 --mysql.version="mysql-8.0.28" --eas.web.address="hhtp://localhost:8060" --eas.address="hhtp://localhost:8060" --eas.embbed.enable=false> eas.log 2>&1 &
-        #nohup $JAVA_HOME/bin/java $JAVA_opens $JAVA_OPTS -classpath $CLASSPATH $APP_MAIN --jdbc.url="jdbc:mysql://172.10.1.10:3306/tiklab_eas?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true" --server.port=8086 --jdbc.username=root --jdbc.password=darth2020 --jdbc.driverClassName="com.mysql.cj.jdbc.Driver" --eas.server.port=8080 --eas.embbed.enable=false --mysql.embbed.enable=true --mysql.name=tiklab_eas --eas.web.address="http:lodalhost:3305" --mysql.server.port=3306 --eas.address="http:lodalhost:3305" --eas.jdbc.url="jdbc:mysql://172.10.1.10:3306/tiklab_matflow?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true" --eas.jdbc.driverClassName="com.mysql.cj.jdbc.Driver" --eas.jdbc.username=root --eas.jdbc.password=darth2020 --mysql.version="mysql-8.0.28" --eas.mysql.embbed.enable=true --eas.mysql.server.port=3306 --eas.mysql.name="tiklab_eas"> eas.log 2>&1 &
-       echo $1
-       nohup $JAVA_HOME/bin/java $JAVA_opens $JAVA_OPTS -classpath $CLASSPATH $APP_MAIN $eas_shell > eas.log 2>&1 &
+       nohup $JAVA_HOME/bin/java $JAVA_opens $JAVA_OPTS -classpath $CLASSPATH $APP_MAIN $eas_shell > $APP_HOME/eas.log 2>&1 &
         # shellcheck disable=SC2034
         for i in $(seq 5)
         do

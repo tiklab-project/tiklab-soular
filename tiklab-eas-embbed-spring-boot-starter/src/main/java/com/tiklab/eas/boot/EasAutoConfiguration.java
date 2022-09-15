46a05ade8bfaed9b1d0d62f306a9bc6d0d2a043e
mahai <mahai@darthcloud.com>
2022-09-15 16:31:55 +0800

    删除文件

--- tiklab-eas-embbed-spring-boot-starter/src/main/java/com/tiklab/eas/boot/EasAutoConfiguration.java
+++ tiklab-eas-embbed-spring-boot-starter/src/main/java/com/tiklab/eas/boot/EasAutoConfiguration.java
@@ -2,10 +2,8 @@ package com.tiklab.eas.boot;
 
 import com.tiklab.dsm.annotation.SQL;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
-@Conditional(EasCondition.class)
 @Configuration
 @SQL(modules = {"portal","easDefaultData"})
 @ComponentScan({"com.tiklab.eas.boot", "com.tiklab.eas"})

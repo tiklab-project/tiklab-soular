656ff7c0db99b22b12451a289772b32cdd8f01e0
mahai <mahai@darthcloud.com>
2022-10-16 16:23:30 +0800

    从eas中抽离applink和widget到pcs中的widgeet中

--- tiklab-eas-starter/src/main/java/net/tiklab/eas/EasAutoConfiguration.java
+++ tiklab-eas-starter/src/main/java/net/tiklab/eas/EasAutoConfiguration.java
@@ -20,6 +20,7 @@ import net.tiklab.todotask.stater.EnableTodoTask;
 import net.tiklab.toolkit.EnableToolkitServer;
 import net.tiklab.user.starter.EnableUser;
 import net.tiklab.web.starter.annotation.EnableWeb;
+import net.tiklab.widget.starter.EnableWidget;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -49,6 +50,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableLicenceServer
 @EnableTodoTask
 @EnableLog
+@EnableWidget
+
 //other
 @EnablePostInClient
 

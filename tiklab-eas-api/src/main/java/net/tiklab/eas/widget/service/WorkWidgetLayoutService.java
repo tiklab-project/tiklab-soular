1c93f0ee20b5750ba8d504e5bd8b45036332ec0f
mahai <mahai@darthcloud.com>
2023-02-24 14:58:51 +0800

    feat:现在weigets

---
+++ tiklab-eas-api/src/main/java/net/tiklab/eas/widget/service/WorkWidgetLayoutService.java
@@ -0,0 +1,17 @@
+package net.tiklab.eas.widget.service;
+
+
+import net.tiklab.eas.widget.model.WorkWidgetLayout;
+import net.tiklab.eas.widget.model.WorkWidgetLayoutQuery;
+
+public interface WorkWidgetLayoutService {
+
+    WorkWidgetLayout findWidgetLayout(WorkWidgetLayoutQuery workWidgetLayoutQuery);
+
+    String createWidgetLayout(WorkWidgetLayout workWidgetLayout);
+
+    void updateWidgetLayout(WorkWidgetLayout workWidgetLayout);
+
+    WorkWidgetLayout findWidgetLayoutOne();
+
+}

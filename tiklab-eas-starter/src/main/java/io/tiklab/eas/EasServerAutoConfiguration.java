e170880e276cdb0ff754d9ecdeddd9576be8d2cb
mahai <mahai@darthcloud.com>
2023-03-08 13:51:02 +0800

    saas 启动成功

--- tiklab-eas-starter/src/main/java/io/tiklab/eas/EasServerAutoConfiguration.java
+++ tiklab-eas-starter/src/main/java/io/tiklab/eas/EasServerAutoConfiguration.java
@@ -1,11 +1,18 @@
 package io.tiklab.eas;
 
-import io.tiklab.dsm.annotation.SQL;
+import io.tiklab.dsm.model.SQL;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SQL(modules = {"eas","easDefaultData"}, order = 100)
 @ComponentScan({"io.tiklab.eas"})
 public class EasServerAutoConfiguration {
+    @Bean
+    SQL initSQL(){
+        return new SQL(new String[] {
+                "eas",
+                "easDefaultData"
+        }, 100);
+    }
 }

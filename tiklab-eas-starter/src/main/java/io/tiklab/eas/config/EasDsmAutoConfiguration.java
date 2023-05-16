ec979221ac3f49fde20dc6f76497133ca12ae928
zhangcheng <zhangcheng@126.com>
2023-05-16 10:43:44 +0800

    op_log 1.0.2

--- tiklab-eas-starter/src/main/java/io/tiklab/eas/config/EasDsmAutoConfiguration.java
+++ tiklab-eas-starter/src/main/java/io/tiklab/eas/config/EasDsmAutoConfiguration.java
@@ -49,6 +49,9 @@ public class EasDsmAutoConfiguration {
                 "user_1.0.1_dml",
                 "oplog_1.0.1_dml"
         });
+        dsmConfig.newVersion("1.0.2", new String[]{
+                "oplog_1.0.2_dml"
+        });
         return dsmConfig;
     }
 

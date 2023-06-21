d9ca38659f513cc85a6d1a01bbf73c8c66eb5982
zhangcheng@darthcloud.com <zcamyy@126.com>
2023-06-21 17:41:11 +0800

    内嵌postgresql

--- tiklab-eas-starter/src/main/java/io/tiklab/eas/EasAutoConfiguration.java
+++ tiklab-eas-starter/src/main/java/io/tiklab/eas/EasAutoConfiguration.java
@@ -12,6 +12,7 @@ import io.tiklab.join.starter.EnableToolkit;
 import io.tiklab.licence.starter.EnableLicenceServer;
 import io.tiklab.messsage.starter.EnableMessage;
 import io.tiklab.pluginx.starter.EnablePluginServer;
+import io.tiklab.postgresql.EnablePostgresql;
 import io.tiklab.privilege.EnablePrivilegeServer;
 import io.tiklab.rpc.starter.annotation.EnableRpc;
 import io.tiklab.security.stater.EnableSecurity;
@@ -25,9 +26,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 //platform
 @EnableToolkit
-
-// @EnableMysql
-
+@EnablePostgresql
 @EnableDal
 @EnableDsm
 @EnableDfs
--- tiklab-eas-starter/src/main/java/io/tiklab/eas/EasServerAutoConfiguration.java
+++ tiklab-eas-starter/src/main/java/io/tiklab/eas/EasServerAutoConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"io.tiklab.eas","io.tiklab.eas.updatesql",})
+@ComponentScan({"io.tiklab.eas"})
 public class EasServerAutoConfiguration {
     @Bean
     SQL initSQL(){

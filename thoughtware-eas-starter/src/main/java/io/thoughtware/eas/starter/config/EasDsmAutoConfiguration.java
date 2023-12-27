6f2716970bd40e940e22803cc54dc1fab9d60a69
zhangcheng <zcamyy@126.com>
2023-12-27 20:13:12 +0800

    Yaml文件更改

--- thoughtware-eas-starter/src/main/java/io/thoughtware/eas/starter/config/EasDsmAutoConfiguration.java
+++ thoughtware-eas-starter/src/main/java/io/thoughtware/eas/starter/config/EasDsmAutoConfiguration.java
@@ -56,10 +56,10 @@ public class EasDsmAutoConfiguration {
                 "oplog_1.0.4",
         });
         dsmConfig.newVersion("1.0.5", new String[]{
-                "message_1.0.4",
+                "message_1.0.5",
         });
         dsmConfig.newVersion("1.0.6", new String[]{
-                "message_1.0.4",
+                "message_1.0.6",
         });
         return dsmConfig;
     }

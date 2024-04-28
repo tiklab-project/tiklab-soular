a9bc0a3bbcbb698af752efb0a39e5ccb44269fdb
zhangcheng <zcamyy@126.com>
2024-04-28 10:53:19 +0800

    启动文件

--- thoughtware-eas-starter/bin/startup_linux.sh
+++ thoughtware-eas-starter/bin/startup_linux.sh
@@ -4,8 +4,8 @@ DIRS=$(dirname "$PWD")
 
 APP_MAIN="io.thoughtware.eas.starter.EasApplication"
 
-
 JDK_VERSION=jdk-16.0.2
+PGSQL_VERSION=pgsql-10.23
 
 if [ -d "${DIRS}/embbed/${JDK_VERSION}" ]; then
     echo "使用内嵌jdk"
@@ -13,6 +13,10 @@ if [ -d "${DIRS}/embbed/${JDK_VERSION}" ]; then
 else
     JAVA_HOME="/usr/local/jdk-17.0.7"
 fi
+
+echo "解压文件....."
+tar -xvf "${DIRS}/embbed/${PGSQL_VERSION}/${PGSQL_VERSION}.tar.gz" -C "${DIRS}/embbed"
+echo "解压完成....."
 
 find ${DIRS}/ -name '*.sh' | xargs dos2unix;
 
